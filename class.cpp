8ef28e08aa42563f7c69b1261ff34f21954fcdcf
2107074 <2107074@kuet.cse>
2025-08-20 16:28:42 +0600

    First class added

---
+++ class.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class c1{
+    int a;
+    c1(){
+    }
+    void setter(int a){
+        this->a = a;
+    }
+    int getter(){
+        return a;
+    }
+};
+int main()
+{
+
+	return 0;
+}
